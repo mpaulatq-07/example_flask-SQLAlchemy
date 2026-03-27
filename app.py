ae894f89a1f75efbfd850f542e87d46dc2b75fca
mpaulatq-07 <mpaula.tq@gmail.com>
2026-03-27 16:08:43 -0500

    correccion de bd para tenerla lista antes de iniciar las rutas, lo cual antes generaba errores

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
-from flask import Flask #importacion de clase flask`
-from routes.contacts import contacts  #importar en Blueprint
+from flask import Flask  # importacion de clase flask
+from routes.contacts import contacts  # importar en Blueprint
+from utils.db import db
+
 
 def create_app():
-    app = Flask(__name__) #Ejecutar flask y que devuelva un objeto,en este caso app
+    app = Flask(__name__)  # Ejecutar flask y que devuelva un objeto,en este caso app
 
-    #pasar parametros para poder configurarlo (nombre bd, contraseña, etc)
+    # pasar parametros para poder configurarlo (nombre bd, contraseña, etc)
+    app.secret_key = "secret_key_1234"
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:12345@localhost/contactsdb'  #a donde queremos concetarnos
-                                                                #propiedad con valor de la direccion de la BD
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        "mysql://root:12345@localhost/contactsdb"  # a donde queremos concetarnos
+    )
+    # propiedad con valor de la direccion de la BD
+    # https://www.youtube.com/watch?v=BP3D03CYFHA&t=2538s
 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #Configuracion flask (por defecto)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = (
+        False  # Configuracion flask (por defecto)
+    )
 
-    #SQLAlchemy(app) #pasar pasa en el modulo el app
+    db.init_app(app)
 
-    app.register_blueprint(contacts) #añadirlo a la app
+    # SQLAlchemy(app) #pasar pasa en el modulo el app
 
-
-
-
- 
+    app.register_blueprint(contacts)  # añadirlo a la app
+    return app
